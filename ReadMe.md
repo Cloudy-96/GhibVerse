ddaac1475ceaac34c78ea3b809e3001058b9d360
unknown <katdemey@outlook.com>
2023-06-06 20:51:07 +0100

    prepared API documentation for f-e use

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# An unofficial Studio Ghibili movie directory
+
+API documentation: <https://ghibliapi.vercel.app/#tag/Films%2Fpaths%2F~1films%2Fget>
+<https://ghibliapi.vercel.app/>
+
+/films
+/people
+/locations
+/species
+/vehicles
--- ghibverse/src/App.jsx
+++ ghibverse/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,6 +8,21 @@ function App() {
     <>
       <div>
         <h1 className="text-3xl font-bold underline">GhibVerse!</h1>
+        <div>
+          <p className="text-sm font-medium text-red-900">search movies</p>
+        </div>
+
+        <main>
+          <h2>Movies</h2>
+          <div>
+            grid display
+            {/* map each movie to load here */}
+          </div>
+        </main>
+
+        <footer >
+          <p className="border-black">Developed by K.C. De Mey</p>
+        </footer>
       </div>
     </>
   );
