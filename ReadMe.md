5a8de990de64e3c775c31b127b7904e7265c932a
unknown <katdemey@outlook.com>
2023-06-29 19:32:04 +0100

    start of sorting functionality implemented. Component folders introduced.

--- ReadMe.md
+++ ReadMe.md
@@ -8,7 +8,7 @@
 
 ## Attributions
 
- Icon: <a href='https://pngtree.com/so/Cute'>Cute png from pngtree.com/</a>
+ <!-- Icon: <a href='https://pngtree.com/so/Cute'>Cute png from pngtree.com/</a> -->
 
  API documentation: <https://ghibliapi.vercel.app/#tag/Films%2Fpaths%2F~1films%2Fget>
 <https://ghibliapi.vercel.app/>
--- ghibverse/src/App.jsx
+++ ghibverse/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0)
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     fetch("https://ghibliapi.vercel.app/films")
@@ -11,35 +11,62 @@ function App() {
       .then((data) => setMovies(data));
   }, []);
 
-  console.log(movies);
+  // TODO: turn into a function when implementing a sort feature
+  movies.sort(function (a, b) {
+    return a.release_date - b.release_date;
+  });
+
+  // console.log("Sorted------", sortedMovies);
+  // movies.sort(movies.title);
+  // console.log("sorted", movies);
 
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">GhibVerse!</h1>
-        <div>
-          <p className="text-sm font-medium text-red-900">search movies</p>
-        </div>
-
+        <header>
+          <h1 className="text-3xl font-bold underline">GhibVerse!</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="search movies"
+              className="text-sm font-medium text-red-900"
+            />
+            <input type="submit" value="search" />
+          </form>
+        </header>
         {/*TODO: implement sort by: title, year of publication asc + desc,rt ratings  */}
 
         <main>
           <h2>Movies</h2>
-          <div>
-            grid display
-            {/* map each movie to load here */}
+          <div className="">
             <ul>
               {movies.map((movie) => (
-                <li key={movie}>
-                  <h3>{movie.title}</h3>
-                  <p>
-                    {movie.original_title} : {movie.original_title_romanised}
-                  </p>
-                  <img src={movie.image} alt="movie poster" />
-                  <p>{movie.producer}</p>
-                  <p>{movie.release_date}</p>
-                  <p>{movie.rt_scorre}</p>
-                  <p>{movie.running_time}</p>
+                <li
+                  key={movie.id}
+                  className="card h-56 border border-gray-900 border-solid rounded m-0.5"
+                >
+                  <div>
+                    <img
+                      className=" card-img h-40 m-1.5"
+                      src={movie.image}
+                      alt="movie poster"
+                    />
+                  </div>
+
+                  <div className="movie-info">
+                    <h3>{movie.title}</h3>
+                    <p>
+                      {movie.original_title} : {movie.original_title_romanised}
+                    </p>
+                    <p>Producer: {movie.producer}</p>
+                    <p>Released: {movie.release_date}</p>
+                    <p>Rotton Tomatoes score: {movie.rt_score}%</p>
+                    <p>Run time: {movie.running_time}</p>
+                  </div>
+
+                  <div className="see-more">
+                    <a href="">See More</a>
+                  </div>
                 </li>
               ))}
             </ul>
